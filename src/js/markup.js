a52f5e48c95d4d494934d1c610aed0a1e46be817
Volodymyr <vladikon70@gmail.com>
2023-07-22 22:14:49 +0200

    update files

--- src/js/markup.js
+++ src/js/markup.js
@@ -2,7 +2,6 @@ import { messageEndCollection, messageError } from './message';
 import { WIDTH, HEIGHT, PER_PAGE } from './const';
 
 function createMarkup(arrData, currentPage, totalHits) {
-  console.log(totalHits);
   if (currentPage > Math.ceil(totalHits / PER_PAGE))
     messageEndCollection(
       "We're sorry, but you've reached the end of search results."
